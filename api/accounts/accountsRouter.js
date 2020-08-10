43cc75f2a41887deb75710768ce3cef4717865f5
Drew Ross <mail.drew.ross@gmail.com>
2020-08-10 13:48:36 -0700

    Create POST /api/accounts endpont.

--- api/accounts/accountsRouter.js
+++ api/accounts/accountsRouter.js
@@ -4,17 +4,26 @@ const db = require("../../data/dbConfig.js");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  db.select('*')
-    .from('accounts')
+  db('accounts')
     .then(accounts => res.status(200).json(accounts))
     .catch(error => {
       console.log(error);
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ message: 'There was an issue getting accounts.', error: error.message });
     });
 });
 
 router.post('/', (req, res) => {
-
+  const newAccount = req.body;
+  db('accounts')
+    .insert(newAccount)
+    .returning("id")
+    .then(ids => {
+      res.status(201).json({ inserted: ids });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'There was an issue adding a new account.', error: error.message})
+    })
 });
 
 router.put('/:id', (req, res) => {

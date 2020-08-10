e1da3dc35dc2d808c1533ec2ef26fb5adf134c08
Drew Ross <mail.drew.ross@gmail.com>
2020-08-10 14:42:05 -0700

    Create middleware to check for body and id. Create endpoint for GET /api/accounts/:id

--- api/accounts/accountsRouter.js
+++ api/accounts/accountsRouter.js
@@ -3,6 +3,39 @@ const db = require("../../data/dbConfig.js");
 
 const router = express.Router();
 
+//middleware
+
+const requireProperties = (keys) => {
+  return (req, res, next) => {
+    const missing = [];
+    keys.forEach(key => {
+      if (!req.body.hasOwnProperty(key)) {
+        missing.push(key);
+      }
+    });
+    if (missing.length > 0) {
+      res.status(400).json({ message: `Please include required properties: ${missing.toString()}` });
+    } else {
+      next();
+    }
+  };
+};
+
+const checkAccountExists = (req, res, next) => {
+  db('accounts')
+    .where({ id: req.params.id })
+    .then(account => {
+      if (account.length > 0) {
+        res.account = account;
+        next();
+      } else {
+        res.status(404).json({ message: 'An account with that id was not found.' });
+      }
+    });
+};
+
+//endpoints
+
 router.get('/', (req, res) => {
   db('accounts')
     .then(accounts => res.status(200).json(accounts))
@@ -12,7 +45,11 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.get('/:id', checkAccountExists, (req, res) => {
+  res.status(200).json(res.account);
+});
+
+router.post('/', requireProperties(['name', 'budget']), (req, res) => {
   const newAccount = req.body;
   db('accounts')
     .insert(newAccount)
@@ -22,12 +59,28 @@ router.post('/', (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      res.status(500).json({ message: 'There was an issue adding a new account.', error: error.message})
-    })
+      res.status(500).json({ message: 'There was an issue adding a new account.', error: error.message });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', requireProperties(['name', 'budget']), (req, res) => {
+  const changes = req.body;
+  const accountId = req.params.id;
 
+  db('accounts')
+    .where({ id: accountId })
+    .update(changes)
+    .then(success => {
+      if (success) {
+        res.status(200).json({ message: 'Updated account successfully.' });
+      } else {
+        res.status(404).json({ message: `An account was not found with id = ${accountId}` });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'There was an issue editing the account.', error: error.message });
+    });
 });
 
 router.delete('/:id', (req, res) => {
